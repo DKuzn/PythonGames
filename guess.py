2cd19dfe78a28db7ccf93bb661075a8c4a833009
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2020-05-22 16:01:08 +0300

    small update

--- guess.py
+++ guess.py
@@ -6,23 +6,23 @@ guessesTaken = 0
 print('Привет! Как тебя зовут?')
 myName = input()
 
-number = rd.randint(1, 20)
-print("Что ж, " + myName + ", я загадываю число от 1 до 20.")
+number = rd.randint(1, 100)
+print("Что ж, " + myName + ", я загадываю число от 1 до 100.")
 
-for guessesTaken in range(6):
+for guessesTaken in range(12):
     print("Попробуй угадать.")
     guess: int = int(input())
 
     if guess < number:
         print("Твое число слишком маленькое.")
     elif guess > number:
-        print("твое число слишком большое.")
+        print("Твое число слишком большое.")
     elif guess == number:
         break
 
 if guess == number:
     guessesTaken = str(guessesTaken + 1)
-    print("Отлично, " + myName + "! ты справился за " + guessesTaken + " попытки!")
+    print("Отлично, " + myName + "! ты справился с " + guessesTaken + " попытки!")
 
 elif guess != number:
     number = str(number)
